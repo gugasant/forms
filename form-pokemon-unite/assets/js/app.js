2795744e51ed8dd2b11eeb36ac1ee24641939506
gugasant <gustavosantlopes3@gmail.com>
2023-01-14 14:06:40 -0300

    firebase app

---
+++ form-pokemon-unite/assets/js/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRuo3r1jRAJuUxa8nKsEiyb7KALk3OALQ",
+  authDomain: "form-pokemon-unite.firebaseapp.com",
+  projectId: "form-pokemon-unite",
+  storageBucket: "form-pokemon-unite.appspot.com",
+  messagingSenderId: "748192009025",
+  appId: "1:748192009025:web:2dd5317719695bac3fefc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
